eaf97bbeb551e680fd5ce47c7a0793160d49b7f8
roman shuralov <guy.lebowski@gmail.com>
2022-08-22 14:32:23 +0300

    test commit

---
+++ app/library/library.py
@@ -0,0 +1,56 @@
+class Book:
+    def __init__(self, genre: str, title: str, author: str):
+        self.title = title
+        self.genre = genre
+        self.author = author
+
+
+class Reader:
+    def __init__(self, firstName: str, lastName: str, iD: int, birthDate: int, telephone: str):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.iD = iD
+        self.birthDate = birthDate
+        self.telephone = telephone
+
+
+class Library:
+    reader = None
+    book = None
+    has_book = {}
+
+    def __init__(self, address: str, telephone: str, books: list, readers: list):
+        self.address = address
+        self.telephone = telephone
+        self.books = books
+        self.readers = readers
+
+    def takeBook(self, reader, book):
+        if book in self.books:
+            self.books.remove(book)
+            self.has_book[book] = reader
+            return f'{reader.firstName} {reader.lastName} took the following book: {book.genre}, {book.title}, {book.author}'
+
+        else:
+            return 'Book is taken'
+
+    def returnBook(self, reader, book):
+        self.books.append(book)
+        self.has_book[book] = None
+        return f'{reader.firstName} {reader.lastName} returned the following book: {book.genre}, {book.title}, {book.author}'
+
+    def book_log(self):
+        for book in self.has_book:
+            return f'{self.has_book[book].lastName} {self.has_book[book].lastName} has {book.title}'
+
+    def write_off_book(self, book):
+        self.books.remove(book)
+
+    def write_on_book(self, book):
+        self.books.append(book)
+
+    def write_off_reader(self, reader):
+        self.readers.remove(reader)
+
+    def write_on_reader(self, reader):
+        self.readers.append(reader)
---
+++ app/server/library_resource.py
@@ -0,0 +1,22 @@
+import json
+from http import HTTPStatus
+from http.server import BaseHTTPRequestHandler
+
+from app.library.library import Library, Book
+
+
+class LibrarvResource:
+    _Library: Library = None
+
+    def __init__(self, Library: Library):
+        self._library = Library
+
+    def add_book(self, req: BaseHTTPRequestHandler):
+        self._Library.add_book(Book("Harry Potter", "Fantasy", "J.K. Rowling"))
+
+        req.send_response(HTTPStatus.OK)
+        req.send_header("Content-type", "application/json")
+        req.end_headers()
+        req.wfile.write(
+            json.dumps({'server_name': 'Simple HTTP server', 'author': 'Andrey Varenyk', 'path': req.path,
+                        'method': req.command, 'Library books': self._library.books.__str__()}).encode())
